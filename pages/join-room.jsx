65adbff8c2579113c93d6ef5abf31ac55df40048
Kevin Nguyen <me@kndwin.dev>
2021-08-23 06:45:31 +1000

    🙏 - pls

--- pages/join-room.jsx
+++ pages/join-room.jsx
@@ -18,7 +18,7 @@ const JoinRoom = (props) => {
   }, []);
 
   const joinRoom = async () => {
-    const URL = `https://roomie.up.railway.app:7525/api/room-exist/${roomId}`;
+    const URL = `http://roomie.up.railway.app/api/room-exist/${roomId}`;
     // const URL = `http://localhost:3001/api/room-exist/${roomId}`;
     const res = await fetch(URL, {
       method: "GET",
@@ -151,7 +151,6 @@ const Error = () => {
           style={{ boxShadow: "7px 7px #7F1D1D" }}
           className="mt-4 w-96 text-center font-bold text-lg text-red-900 
 					border-red-900 border-2 my-2 p-2 rounded-lg relative"
-        >
           {error}
         </div>
       )}
